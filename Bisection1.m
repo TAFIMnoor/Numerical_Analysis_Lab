519536cd8d0ebb9f3f6934caa3591b4843255b83
TAFIMnoor <43815369+TAFIMnoor@users.noreply.github.com>
2019-10-30 08:34:18 +0600

    Add files via upload
    Bisection method using octave

---
+++ Bisection1.m
@@ -0,0 +1,18 @@
+a =2;
+b =3;
+f= @(x) x^3-2*x^2-4;
+eps = 1e-6;
+no_iterations=0;
+    solution = a;
+    while abs(b - a) >= eps
+        solution = (a + b) / 2;
+        no_iterations = no_iterations + 1;
+        if f(solution) == 0
+            break;
+        elseif (f(a) * f(solution)) < 0
+            b = solution;
+        else
+            a = solution;
+        end;
+endwhile
+solution
